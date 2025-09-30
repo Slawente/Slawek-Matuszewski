e2b6b4be7234dc3ea212b77d84f27684fc0274cb
Slawente <slawek1-mat@wp.pl>
2025-09-30 14:20:07 +0200

    Add files via upload

---
+++ ewq.py
@@ -0,0 +1,15 @@
+class Druzyny:
+    def __init__(self, a, b):
+        self.nazwa = a
+        self.punkty = b
+
+class Liga:
+    def __init__(self, a):
+        self.teams = []
+        self.teams = a
+
+    def TopTeams(self):
+        
+
+Druzyna = Druzyny(3, 'Amicus Mórka')
+
